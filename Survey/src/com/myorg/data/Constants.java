cf2557c25cf7d3ecb93ab4d1e2b8a8e948bed2c9
unknown <z000dg6m@G293Q1S.nsn-intra.net>
2012-02-19 14:19:35 +0530

    Created first version of Vehicle Analyser
    Created for the Coding Challenge

---
+++ Survey/src/com/myorg/data/Constants.java
@@ -0,0 +1,19 @@
+package com.myorg.data;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.myorg.data.Enums.Day;
+
+public class Constants {
+	
+   public static String[] DAYSOFWEEK = 
+   {
+	   "MONDAY", 
+	   "TUESDAY", 
+	   "WEDNESDAY",
+	   "THURSDAY",
+	   "FRIDAY"
+   };
+   
+}
