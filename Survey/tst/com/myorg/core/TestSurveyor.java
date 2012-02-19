894dc1f0ce4d15d34ef7cae9501034c244307577
unknown <z000dg6m@G293Q1S.nsn-intra.net>
2012-02-19 14:20:07 +0530

    Created first version of Vehicle Analyser
    Created for the Coding Challenge

---
+++ Survey/tst/com/myorg/core/TestSurveyor.java
@@ -0,0 +1,37 @@
+package com.myorg.core;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestSurveyor {
+	
+	 
+	/*@Test
+	public void testCountForNorthBound() {
+ 
+		Surveyor surveyor = new Surveyor();
+		surveyor.create();
+		Assert.assertEquals(surveyor.getCountForNorthBound() , 2);
+ 
+	}
+*/
+/*	@Test
+	public void testgetCountOfcarsInMorning() {
+ 
+		Surveyor surveyor = new Surveyor();
+		surveyor.create();
+		
+		Assert.assertEquals(surveyor.getCountOfcarsInMorning(), 3);
+ 
+	}
+*/
+	@Test
+	public void testgetCountOfCarsInTimeSpansofEveryHour(){
+		
+		Surveyor.create();
+		
+		Surveyor.getCountOfCarsInTimeSpansofEveryFifteenMinutesNorthBound(); 
+	}
+	
+}
